2cc74a38c2d531682fbc710ab14813b333481b80
mosguinz <mos.guinz@gmail.com>
2024-07-27 19:32:16 -0700

    Update script to run part A

--- run_pa.sh
+++ run_pa.sh
@@ -1,1 +1,16 @@
 ./a.out > pa_stdout.txt
+
+#!/bin/zsh
+setopt nullglob
+
+g++ -std=c++17 -- *[aA]/*.cpp *[aA]/*.h
+
+./a.out > a.txt
+vimdiff -c "set diffopt+=iwhiteall foldlevel=99999" a.txt pa_stdout_1.txt
+rm a.txt
+
+./a.out > a.txt
+vimdiff -c "set diffopt+=iwhiteall foldlevel=99999" a.txt pa_stdout_2.txt
+rm a.txt
+
+rm a.out
