ac8d1b30be32ae4ddc620c503bacfc70a93efb36
mosguinz <mos.guinz@gmail.com>
2024-07-29 14:06:42 -0700

    Add option to rerun in case of changes

--- run_pa.sh
+++ run_pa.sh
@@ -1,17 +1,41 @@
 #!/bin/zsh
 setopt nullglob
 
-#cp *[aA]/{Include.h,LinkedBag340.cpp} Assignment-04-Code/
-#g++ -std=c++17 Assignment-04-Code/*.h Assignment-04-Code/*.cpp
+cp -- *[aA]/{Include.h,LinkedBag340.cpp} Assignment-04-Code/
+g++ -std=c++17 Assignment-04-Code/*.h Assignment-04-Code/*.cpp
 
-g++ -std=c++17 *[aA]/{Include.h,LinkedBag340.cpp} Assignment-04-Code/*.{cpp,h}
+run_diff() {
+  ./a.out > a.txt
+  vimdiff -c "set diffopt+=iwhiteall foldlevel=99999" a.txt pa_stdout_1.txt
+  rm a.txt
 
-./a.out > a.txt
-vimdiff -c "set diffopt+=iwhiteall foldlevel=99999" a.txt pa_stdout_1.txt
-rm a.txt
+  ./a.out > a.txt
+  vimdiff -c "set diffopt+=iwhiteall foldlevel=99999" a.txt pa_stdout_2.txt
+  rm a.txt
+}
 
-./a.out > a.txt
-vimdiff -c "set diffopt+=iwhiteall foldlevel=99999" a.txt pa_stdout_2.txt
-rm a.txt
+run_diff
 
-rm a.out
+while true; do
+  echo "1: Rerun with last outfile"
+  echo "2: Recompile and run with student submitted file"
+  echo "3: Exit"
+  echo -n "Select (1-3): "
+  read choice
+
+  case $choice in
+    1)
+      run_diff
+      ;;
+    2)
+      g++ -std=c++17 -- *[aA]/*.{cpp,h}
+      run_diff
+      ;;
+    3)
+      break
+      ;;
+  esac
+done
+
+rm -f a.out a.txt
+
