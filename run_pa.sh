ca7468d68dd8e306bdc76b5b6c3e7a575a9afba6
mosguinz <mos.guinz@gmail.com>
2024-07-29 13:44:00 -0700

    Update script to use starter files only

--- run_pa.sh
+++ run_pa.sh
@@ -1,9 +1,10 @@
-./a.out > pa_stdout.txt
-
 #!/bin/zsh
 setopt nullglob
 
-g++ -std=c++17 -- *[aA]/*.cpp *[aA]/*.h
+#cp *[aA]/{Include.h,LinkedBag340.cpp} Assignment-04-Code/
+#g++ -std=c++17 Assignment-04-Code/*.h Assignment-04-Code/*.cpp
+
+g++ -std=c++17 *[aA]/{Include.h,LinkedBag340.cpp} Assignment-04-Code/*.{cpp,h}
 
 ./a.out > a.txt
 vimdiff -c "set diffopt+=iwhiteall foldlevel=99999" a.txt pa_stdout_1.txt
